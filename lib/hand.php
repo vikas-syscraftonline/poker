0a14237ccd215cbc51c3471dd2e9c31010335a36
Jason Brown <j.brown@ctidigital.com>
2012-09-11 14:14:21 +0100

    added class hand

---
+++ lib/hand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Poker;
+
+/**
+ * Poker Hand
+ *
+ * @author Jason Brown <jason.brown@jbfreelance.co.uk>
+ */
+class Hand
+{
+    protected $cards;
+    
+    public function __construct()
+    {
+        $this->cards = array();
+    }
+}
+
+?>
