3b7a1967921d8decf575807ba513a6a38e3f2581
Jason Brown <j.brown@ctidigital.com>
2012-09-11 18:05:55 +0100

    added functions to add and get cards

--- lib/hand.php
+++ lib/hand.php
@@ -15,6 +15,34 @@ class Hand
     {
         $this->cards = array();
     }
+    
+    /**
+     * @param Card $Card
+     * @throws Exception
+     */
+    public function add(Card $Card)
+    {
+        if (in_array($Card, $this->cards)) {
+            throw new \Exception('Cannot contain the same card twice!');
+        } else {
+            //@todo num cards in hand against a set of rules
+            if(count($this->cards) < 5)
+            {
+                $this->cards[] = $Card;
+            }else{
+                throw new \Exception('Too many cards for this hand, do you have an one up your sleeve?');
+            }
+        }
+    }
+    
+    /**
+     * Returns Array of Cards
+     * @return array 
+     */
+    public function getCards()
+    {
+        return $this->cards;
+    }
 }
 
 ?>
